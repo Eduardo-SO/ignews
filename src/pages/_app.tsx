eb750f0fceb99a85aa2212d3b50e99fcd7b0b453
Eduardo-SO <eduardo.sool07@gmail.com>
2021-03-27 13:36:44 -0300

    Add Header component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,15 @@
 import { AppProps } from 'next/app'
+
+import { Header } from '../components/Header'
 import '../styles/global.scss'
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Header />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
